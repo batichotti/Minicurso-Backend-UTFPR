42658aaca60852d00dd814aa5bea60d6b384a611
Matt Batichotti <52006006+MateusCohuzer@users.noreply.github.com>
2024-08-28 20:47:07 -0300

    nest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Get('hello')
   getHello(): string {
     return this.appService.getHello();
   }
-}
+  @Get('goodbye')
+  getGoodbye(): string {
+    return this.appService.getHello();
+  }
+ }
